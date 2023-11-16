0346b2ed91949d661bfbabd19cec9f076b5bf3d3
Austin Doty <asrdoty@gmail.com>
2023-11-16 15:09:04 -0500

    Migrations Coded

---
+++ src/db/migrations/20231116150519_migrateMovies.js
@@ -0,0 +1,15 @@
+exports.up = function (knex) {
+    return knex.schema.createTable("movies", (table) => {
+      table.increments("movie_id").primary();
+      table.integer("runtime_in_minutes");
+      table.text("description");
+      table.string("title");
+      table.string("rating");
+      table.string("image_url");
+      table.timestamps(true, true);
+    });
+  };
+  
+  exports.down = function (knex) {
+    return knex.schema.dropTable("movies");
+  };
