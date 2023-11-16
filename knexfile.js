dac17a5ed6a003d07edc8a923f089240bd2cec69
Austin Doty <asrdoty@gmail.com>
2023-11-16 15:37:17 -0500

    Fixed Connection to ElephantSQL

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "suleiman.db.elephantsql.com",
 } = process.env;
 
 module.exports = {
