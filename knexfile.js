8087d6b38f87b8ff47b2349ee5d32a03acadd91c
Austin Doty <asrdoty@gmail.com>
2023-11-16 17:12:58 -0500

    final bug and secuirty fix

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,7 @@ const path = require("path");
 
 require("dotenv").config();
 
-const {
-  DATABASE_URL = "...",
-} = process.env;
+const { DATABASE_URL } = process.env;
 
 module.exports = {
   development: {
