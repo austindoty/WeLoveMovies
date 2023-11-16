fc052b7c884d8a4157414620a0d5060b99bcb8c1
Austin Doty <asrdoty@gmail.com>
2023-11-16 14:32:19 -0500

    AppJS Routes Completed

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const express = require("express");
 const cors = require("cors");
 const errorHandler = require("./utils/errors/errorHandler");
 const notFound = require("./utils/errors/notFound");
+const reviewsRouter = require("./reviews/reviews.router");
+const moviesRouter = require("./movies/movies.router");
+const theatersRouter = require("./theaters/theaters.router");
 
 const app = express();
 
