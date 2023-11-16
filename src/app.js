f374067847355faab82590f3cdbd0c2e9c7e1458
Austin Doty <asrdoty@gmail.com>
2023-11-16 14:09:36 -0500

    errors&their routes added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 if (process.env.USER) require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
+const errorHandler = require("./utils/errors/errorHandler");
+const notFound = require("./utils/errors/notFound");
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/reviews", reviewsRouter);
+app.use("/movies", moviesRouter);
+app.use("/theaters", theatersRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
